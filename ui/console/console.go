f1b164a96e7da6cc9dc724c5de415078b7a29f0a
the-only-cookie <the.only.cookie.lera@gmail.com>
2024-05-03 09:39:51 +0300

    commit message

--- ui/console/console.go
+++ ui/console/console.go
@@ -1,17 +1,31 @@
 package console
 
 import (
+	"bufio"
 	"fmt"
 	"main/logic"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func readNum() int {
-	num, err := fmt.Scanln()
+	in := bufio.NewReader(os.Stdin)
+	str, err := in.ReadString('\n')
 	if err != nil {
-		showMessage(err.Error())
+		showMessage("error: " + err.Error())
+	}
+
+	num, err := strconv.Atoi(formatInput(str))
+	if err != nil {
+		showMessage("error: " + err.Error())
 	}
 
 	return num
+}
+
+func formatInput(input string) string {
+	return strings.TrimSpace(strings.Trim(input, "\n"))
 }
 
 func showMessage(message any) {
